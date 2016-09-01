2454bae78ef17d4f8bc603d55168ecd0524e3320
Stephen Chang <stchang@ccs.neu.edu>
2016-09-01 14:59:57 -0400

    add optimize, remaining sec3 tests; fix CList-CListof subtyping

--- turnstile/examples/tests/rosette/run-all-rosette-tests.rkt
+++ turnstile/examples/tests/rosette/run-all-rosette-tests.rkt
@@ -1,7 +1,10 @@
 #lang racket/base
-(require "rosette-tests.rkt")
+(require "rosette2-tests.rkt")
 (require "bv-tests.rkt")
+(require "rosette-guide-sec2-tests.rkt")
+(require "rosette-guide-sec3-tests.rkt")
+
 ;(require "bv-ref-tests.rkt")
 ; visit but dont instantiate, o.w. will get unsat
 ;(dynamic-require "fsm-test.rkt" #f)
-(require "ifc-tests.rkt")
+;(require "ifc-tests.rkt")
